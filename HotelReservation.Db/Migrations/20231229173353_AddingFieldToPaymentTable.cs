07390f09017cabe0a87815dc0bcebf2df3cd4459
unknown <amrsalam@hotmail.com>
2023-12-30 14:09:47 +0200

    Creating Database library and Add migration to reflect it on the Database

---
+++ HotelReservation.Db/Migrations/20231229173353_AddingFieldToPaymentTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelReservation.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingFieldToPaymentTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Amount",
+                table: "Payments",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "Payments");
+        }
+    }
+}
