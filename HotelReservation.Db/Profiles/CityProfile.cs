5aee529491943e6cbb929561b4fef56735c0dddd
unknown <amrsalam@hotmail.com>
2023-12-30 22:23:50 +0200

    Creating City Repository

---
+++ HotelReservation.Db/Profiles/CityProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using HotelReservation.Db.Models;
+
+namespace HotelReservation.Db.Profiles
+{
+    public class CityProfile : Profile
+    {
+        public CityProfile()
+        {
+            CreateMap<Domain.Models.City, City>();
+
+            CreateMap<City, Domain.Models.City>();
+        }
+    }
+}
