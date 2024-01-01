5d72f120bdb9487efcc9ff0a48902a3f30b020dd
unknown <amrsalam@hotmail.com>
2024-01-01 15:31:43 +0200

    Adding an extension method for all services

---
+++ HotelReservation.Application/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using HotelReservation.Application.Services;
+using HotelReservation.Domain.ServiceInterfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HotelReservation.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICityService , CityService>();
+            services.AddScoped<IHotelService , HotelService>();
+            services.AddScoped<IFeaturedDealService , FeaturedDealService>();
+            services.AddScoped<IUserService , UserService>();
+            services.AddScoped<IRoomService , RoomService>();
+            services.AddScoped<IReservationService , ReservationService>();
+            services.AddScoped<IPaymentService , PaymentService>();
+
+            return services;
+        }
+    }
+}
