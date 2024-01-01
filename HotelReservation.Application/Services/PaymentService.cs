a1ed27dfa2b9a74b737d7899a2271c251d34abac
unknown <amrsalam@hotmail.com>
2024-01-01 15:04:41 +0200

    Adding Payment Service

---
+++ HotelReservation.Application/Services/PaymentService.cs
@@ -0,0 +1,46 @@
+﻿using HotelReservation.Domain.Models;
+using HotelReservation.Domain.RepositoryInterfaces;
+using HotelReservation.Domain.ServiceInterfaces;
+
+namespace HotelReservation.Application.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        private readonly IPaymentRepository _paymentRepository;
+
+        public PaymentService(IPaymentRepository paymentRepository)
+        {
+            _paymentRepository = paymentRepository;
+        }
+
+        public async Task AddPaymentAsync(Payment payment)
+        {
+            await _paymentRepository.AddPaymentAsync(payment);
+        }
+
+        public async Task DeletePaymentAsync(int paymentId)
+        {
+            await _paymentRepository.DeletePaymentAsync(paymentId);
+        }
+
+        public async Task<List<Payment>> GetAllPaymentsAsync()
+        {
+            return await _paymentRepository.GetAllPaymentsAsync();
+        }
+
+        public async Task<Payment> GetPaymentByIdAsync(int paymentId)
+        {
+            return await _paymentRepository.GetPaymentByIdAsync(paymentId);
+        }
+
+        public async Task<bool> PaymentExists(int paymentId)
+        {
+            return await _paymentRepository.PaymentExists(paymentId);
+        }
+
+        public async Task UpdatePaymentAsync(Payment payment)
+        {
+            await _paymentRepository.UpdatePaymentAsync(payment);
+        }
+    }
+}
