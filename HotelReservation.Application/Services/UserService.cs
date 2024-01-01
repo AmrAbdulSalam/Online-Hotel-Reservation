580b5fe3a5edebed910acad51530575a410aa3c4
unknown <amrsalam@hotmail.com>
2024-01-01 15:18:52 +0200

    Adding User Service

---
+++ HotelReservation.Application/Services/UserService.cs
@@ -0,0 +1,46 @@
+﻿using HotelReservation.Domain.Models;
+using HotelReservation.Domain.RepositoryInterfaces;
+using HotelReservation.Domain.ServiceInterfaces;
+
+namespace HotelReservation.Application.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task AddUserAsync(User user)
+        {
+            await _userRepository.AddUserAsync(user);
+        }
+
+        public async Task DeleteUserAsync(int userId)
+        {
+            await _userRepository.DeleteUserAsync(userId);
+        }
+
+        public async Task<List<User>> GetAllUsersAsync()
+        {
+            return await _userRepository.GetAllUsersAsync();
+        }
+
+        public async Task<User> GetUserByIdAsync(int userId)
+        {
+            return await _userRepository.GetUserByIdAsync(userId);
+        }
+
+        public Task UpdateUserAsync(User user)
+        {
+            return _userRepository.UpdateUserAsync(user);
+        }
+
+        public async Task<bool> UserExists(int userId)
+        {
+            return await _userRepository.UserExists(userId);
+        }
+    }
+}
