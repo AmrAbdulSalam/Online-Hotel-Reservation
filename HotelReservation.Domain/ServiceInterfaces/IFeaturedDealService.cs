b5d1e1f7f207ed04ca388ba2d291da7dc6ad987a
unknown <amrsalam@hotmail.com>
2024-01-01 13:10:33 +0200

    Creating domain Featured Deal Service

---
+++ HotelReservation.Domain/ServiceInterfaces/IFeaturedDealService.cs
@@ -0,0 +1,19 @@
+﻿using HotelReservation.Domain.Models;
+
+namespace HotelReservation.Domain.ServiceInterfaces
+{
+    public interface IFeaturedDealService
+    {
+        Task<List<FeaturedDeal>> GetAllFeaturedDealsAsync();
+
+        Task<FeaturedDeal> GetFeaturedDealByIdAsync(int featuredDealId);
+
+        Task AddFeaturedDealAsync(FeaturedDeal featuredDeal);
+
+        Task UpdateFeaturedDealAsync(FeaturedDeal featuredDeal);
+
+        Task DeleteFeaturedDealAsync(int featuredDealId);
+
+        Task<bool> FeaturedDealExists(int featuredDealId);
+    }
+}
