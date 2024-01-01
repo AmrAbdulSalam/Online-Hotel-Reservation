1484d32c7ee738d6ec94b750f12257d0e4a3c719
unknown <amrsalam@hotmail.com>
2024-01-01 13:07:11 +0200

    Creating domain City Service

---
+++ HotelReservation.Domain/ServiceInterfaces/ICityService.cs
@@ -0,0 +1,19 @@
+﻿using HotelReservation.Domain.Models;
+
+namespace HotelReservation.Domain.ServiceInterfaces
+{
+    public interface ICityService
+    {
+        Task<List<City>> GetAllCitiesAsync();
+
+        Task<City> GetCityByIdAsync(int cityId);
+
+        Task AddCityAsync(City city);
+
+        Task UpdateCityAsync(City city);
+
+        Task DeleteCityAsync(int cityId);
+
+        Task<bool> CityExists(int cityId);
+    }
+}
