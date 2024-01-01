18552dbaa6bfb399d1b5fd43c3232c074d947163
unknown <amrsalam@hotmail.com>
2024-01-01 15:09:38 +0200

    Adding Reservation Service

---
+++ HotelReservation.Application/Services/ReservationService.cs
@@ -0,0 +1,46 @@
+﻿using HotelReservation.Domain.Models;
+using HotelReservation.Domain.RepositoryInterfaces;
+using HotelReservation.Domain.ServiceInterfaces;
+
+namespace HotelReservation.Application.Services
+{
+    public class ReservationService : IReservationService
+    {
+        private readonly IReservationRepository _reservationRepository;
+
+        public ReservationService(IReservationRepository reservationRepository)
+        {
+            _reservationRepository = reservationRepository;
+        }
+
+        public async Task AddReservationAsync(Reservation reservation)
+        {
+            await _reservationRepository.AddReservationAsync(reservation);
+        }
+
+        public async Task DeleteReservationAsync(int reservationId)
+        {
+            await _reservationRepository.DeleteReservationAsync(reservationId);
+        }
+
+        public async Task<List<Reservation>> GetAllReservationsAsync()
+        {
+            return await _reservationRepository.GetAllReservationsAsync();
+        }
+
+        public async Task<Reservation> GetReservationByIdAsync(int reservationId)
+        {
+            return await _reservationRepository.GetReservationByIdAsync(reservationId);
+        }
+
+        public async Task<bool> ReservationExists(int reservationId)
+        {
+            return await _reservationRepository.ReservationExists(reservationId);
+        }
+
+        public async Task UpdateReservationAsync(Reservation reservation)
+        {
+            await _reservationRepository.UpdateReservationAsync(reservation);
+        }
+    }
+}
