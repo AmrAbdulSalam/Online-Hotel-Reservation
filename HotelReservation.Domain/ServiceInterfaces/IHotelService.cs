eb5d53202235040c63eb7655c507538242d0ac8c
unknown <amrsalam@hotmail.com>
2024-01-01 13:13:48 +0200

    Creating domain Hotel Service

---
+++ HotelReservation.Domain/ServiceInterfaces/IHotelService.cs
@@ -0,0 +1,19 @@
+﻿using HotelReservation.Domain.Models;
+
+namespace HotelReservation.Domain.ServiceInterfaces
+{
+    public interface IHotelService
+    {
+        Task<List<Hotel>> GetAllHotelsAsync();
+
+        Task<Hotel> GetHotelByIdAsync(int hotelId);
+
+        Task AddHotelAsync(Hotel hotel);
+
+        Task UpdateHotelAsync(Hotel hotel);
+
+        Task DeleteHotelAsync(int hotelId);
+
+        Task<bool> HotelExists(int hotelId);
+    }
+}
