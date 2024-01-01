7ac660e031bf9a070d7ddcf3a3250157ec9191c0
unknown <amrsalam@hotmail.com>
2024-01-01 13:20:09 +0200

    Creating domain Room Service

---
+++ HotelReservation.Domain/ServiceInterfaces/IRoomService.cs
@@ -0,0 +1,19 @@
+﻿using HotelReservation.Domain.Models;
+
+namespace HotelReservation.Domain.ServiceInterfaces
+{
+    public interface IRoomService
+    {
+        Task<List<Room>> GetAllRoomsAsync();
+
+        Task<Room> GetRoomByIdAsync(int roomId);
+
+        Task AddRoomAsync(Room room);
+
+        Task UpdateRoomAsync(Room room);
+
+        Task DeleteRoomAsync(int roomId);
+
+        Task<bool> RoomExists(int roomId);
+    }
+}
