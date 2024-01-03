c705833b723dbd7144d7797aaa277580e9b3b5df
unknown <amrsalam@hotmail.com>
2024-01-03 18:47:14 +0200

    Creating CRUD operations and Validations for Hotel Controller

---
+++ HotelReservation.Api/Models/HotelDTO.cs
@@ -0,0 +1,13 @@
+﻿
+namespace HotelReservation.Api.Models
+{
+    public class HotelDTO
+    {
+        public string Name { get; set; }
+        public double StarRate { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public IFormFile Image { get; set; }
+        public int CityId { get; set; }
+    }
+}
