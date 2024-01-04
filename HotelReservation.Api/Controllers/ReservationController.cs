e80587be25dca13385d974a338c6b61d4bc9d9a2
unknown <amrsalam@hotmail.com>
2024-01-04 17:43:05 +0200

    Creating CRUD operations and Validations for Reservation Controller

---
+++ HotelReservation.Api/Controllers/ReservationController.cs
@@ -0,0 +1,157 @@
+﻿using AutoMapper;
+using FluentValidation;
+using HotelReservation.Api.Models;
+using HotelReservation.Application.Services;
+using HotelReservation.Domain.Models;
+using HotelReservation.Domain.ServiceInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelReservation.Api.Controllers
+{
+    [Route("api/reservations")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        private readonly IReservationService _reservationService;
+        private readonly IMapper _mapper;
+        private readonly IValidator<ReservationDTO> _validator;
+        private readonly IRoomService _roomService;
+
+        public ReservationController(IReservationService reservationService, IMapper mapper, 
+            IValidator<ReservationDTO> validator, IRoomService roomService, IFeaturedDealService featuredDealService)
+        {
+            _reservationService = reservationService ?? throw new ArgumentNullException(nameof(reservationService));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _validator = validator ?? throw new ArgumentNullException(nameof(validator));
+            _roomService = roomService ?? throw new ArgumentNullException(nameof(roomService));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Reservation>>> GetAllReservationsAsync()
+        {
+            return Ok(await _reservationService.GetAllReservationsAsync());
+        }
+
+        [HttpGet("{reservationId}" , Name = "GetReservationById")]
+        public async Task<ActionResult<Reservation>> GetReservationByIdAsync(int reservationId)
+        {
+            var reservationExists = await _reservationService.ReservationExists(reservationId);
+
+            if (!reservationExists)
+            {
+                return NotFound($"Reservation with ID {reservationId} not found");
+            }
+
+            return Ok(await _reservationService.GetReservationByIdAsync(reservationId));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Reservation>> AddReservationAsync(ReservationDTO newReservation)
+        {
+            var validationResult = await _validator.ValidateAsync(newReservation);
+
+            if (!validationResult.IsValid)
+            {
+                var errors = validationResult.Errors.Select(error =>
+                new
+                {
+                    Property = error.PropertyName,
+                    Error = error.ErrorMessage
+                })
+                .ToList();
+
+                return BadRequest(new { Errors = errors });
+            }
+
+            var room = await _roomService.GetRoomByIdAsync(newReservation.RoomId);
+
+            if (!room.Available)
+            {
+                return BadRequest("Room Not Available!!");
+            }
+
+            var availableDate = await _reservationService.IsReservationAvailableAsync(newReservation.RoomId, newReservation.CheckIn, newReservation.CheckOut);
+
+            if (!availableDate)
+            {
+                return BadRequest("Room is reserved in this period please change Dates!!");
+            }
+
+            var mappedReservation = _mapper.Map<Reservation>(newReservation);
+
+            mappedReservation.UpdatePrice(room.PricePerNight, 0);
+
+            mappedReservation.Id = await _reservationService.AddReservationAsync(mappedReservation);
+
+            return CreatedAtRoute("GetReservationById",
+                new
+                {
+                    reservationId = mappedReservation.Id
+                },
+                mappedReservation);
+        }
+
+        [HttpDelete("{reservationId}")]
+        public async Task<ActionResult> DeleteReservationAsync(int reservationId)
+        {
+            var reservationExists = await _reservationService.ReservationExists(reservationId);
+
+            if (!reservationExists)
+            {
+                return NotFound($"FeaturedDeal with ID {reservationId} not found");
+            }
+
+            await _reservationService.DeleteReservationAsync(reservationId);
+
+            return NoContent();
+        }
+
+        [HttpPut("{reservationId}")]
+        public async Task<ActionResult> UpdateReservationAsync(int reservationId, ReservationDTO updatedReservation)
+        {
+            var reservationExists = await _reservationService.ReservationExists(reservationId);
+
+            if (!reservationExists)
+            {
+                return NotFound($"Reservation with ID {reservationId} not found");
+            }
+
+            var validationResult = await _validator.ValidateAsync(updatedReservation);
+
+            if (!validationResult.IsValid)
+            {
+                var errors = validationResult.Errors.Select(error =>
+                new
+                {
+                    Property = error.PropertyName,
+                    Error = error.ErrorMessage
+                })
+                .ToList();
+
+                return BadRequest(new { Errors = errors });
+            }
+
+            var room = await _roomService.GetRoomByIdAsync(updatedReservation.RoomId);
+
+            if (!room.Available)
+            {
+                return BadRequest("Room Not Available!!");
+            }
+
+            var availableDate = await _reservationService.IsReservationAvailableAsync(updatedReservation.RoomId, updatedReservation.CheckIn, updatedReservation.CheckOut);
+
+            if (!availableDate)
+            {
+                return BadRequest("Room is reserved in this period please change Dates!!");
+            }
+
+            var reservation = await _reservationService.GetReservationByIdAsync(reservationId);
+
+            _mapper.Map(updatedReservation, reservation);
+
+            await _reservationService.UpdateReservationAsync(reservation);
+
+            return NoContent();
+        }
+    }
+}
---
+++ HotelReservation.Api/Models/ReservationDTO.cs
@@ -0,0 +1,12 @@
+﻿
+namespace HotelReservation.Api.Models
+{
+    public class ReservationDTO
+    {
+        public bool IsFeaturedDeal { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public int UserId { get; set; }
+        public int RoomId { get; set; }
+    }
+}
