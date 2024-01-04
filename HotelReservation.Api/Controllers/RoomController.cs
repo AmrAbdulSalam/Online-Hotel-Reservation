de60d51080a977068010f3f5ec4acb01a449aee4
unknown <amrsalam@hotmail.com>
2024-01-04 12:15:21 +0200

    Creating CRUD operations and Validations for Room Controller

---
+++ HotelReservation.Api/Controllers/RoomController.cs
@@ -0,0 +1,174 @@
+﻿using AutoMapper;
+using FluentValidation;
+using HotelReservation.Api.Models;
+using HotelReservation.Domain.Models;
+using HotelReservation.Domain.ServiceInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelReservation.Api.Controllers
+{
+    [Route("api/rooms")]
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+        private readonly IRoomService _roomService;
+        private readonly IMapper _mapper;
+        private readonly IValidator<RoomDTO> _validator;
+        private readonly IConfiguration _configuration;
+        private string timestamp = DateTime.Now.ToString("yyyyMMddHHmmss");
+
+        public RoomController(IRoomService roomService, IMapper mapper, IValidator<RoomDTO> validator, IConfiguration configuration)
+        {
+            _roomService = roomService ?? throw new ArgumentNullException(nameof(roomService));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _validator = validator ?? throw new ArgumentNullException(nameof(validator));
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Room>>> GetAllRoomsAsync()
+        {
+            return Ok(await _roomService.GetAllRoomsAsync());
+        }
+
+        [HttpGet("{roomId}" , Name = "GetRoomById")]
+        public async Task<ActionResult<Room>> GetRoomByIdAsync(int roomId)
+        {
+            var roomExists = await _roomService.RoomExists(roomId);
+
+            if (!roomExists)
+            {
+                return NotFound($"Room with ID {roomId} not found");
+            }
+
+            return Ok(await _roomService.GetRoomByIdAsync(roomId));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Room>> AddRoomAsync([FromForm] RoomDTO newRoom)
+        {
+            var validationResult = await _validator.ValidateAsync(newRoom);
+
+            if (!validationResult.IsValid)
+            {
+                var errors = validationResult.Errors.Select(error =>
+                new
+                {
+                    Property = error.PropertyName,
+                    Error = error.ErrorMessage
+                })
+                .ToList();
+
+                return BadRequest(new { Errors = errors });
+            }
+
+            try
+            {
+                var fileExtension = Path.GetExtension(newRoom.Image.FileName);
+
+                var fileName = $"{newRoom.RoomNumber}_{timestamp}{fileExtension}";
+
+                var directoryPath = _configuration.GetValue<string>("ImagePath:DirectoryPath");
+
+                var path = Path.Combine(directoryPath, fileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    newRoom.Image.CopyTo(stream);
+                }
+
+                var mappedRoom = _mapper.Map<Room>(newRoom);
+
+                mappedRoom.Image = path;
+                mappedRoom.Id = await _roomService.AddRoomAsync(mappedRoom);
+
+                return CreatedAtRoute("GetRoomById",
+                    new
+                    {
+                        roomId = mappedRoom.Id
+                    },
+                    mappedRoom);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{roomId}")]
+        public async Task<ActionResult> DeleteRoomAsync(int roomId)
+        {
+            var roomExists = await _roomService.RoomExists(roomId);
+
+            if (!roomExists)
+            {
+                return NotFound($"Room with ID {roomId} not found");
+            }
+
+            await _roomService.DeleteRoomAsync(roomId);
+
+            return NoContent();
+        }
+
+        [HttpPut("{roomId}")]
+        public async Task<ActionResult> UpdateRoomAsync(int roomId, [FromForm] RoomDTO updatedRoom)
+        {
+            var roomExists = await _roomService.RoomExists(roomId);
+
+            if (!roomExists)
+            {
+                return NotFound($"Room with ID {roomId} not found");
+            }
+
+            var validationResult = await _validator.ValidateAsync(updatedRoom);
+
+            if (!validationResult.IsValid)
+            {
+                var errors = validationResult.Errors.Select(error =>
+                new
+                {
+                    Property = error.PropertyName,
+                    Error = error.ErrorMessage
+                })
+                .ToList();
+
+                return BadRequest(new { Errors = errors });
+            }
+
+            var room = await _roomService.GetRoomByIdAsync(roomId);
+
+            _mapper.Map(updatedRoom, room);
+
+            room.UpdateModificationDate();
+
+            try
+            {
+                if (updatedRoom.Image != null)
+                {
+                    var fileExtension = Path.GetExtension(updatedRoom.Image.FileName);
+
+                    var fileName = $"{updatedRoom.RoomNumber}_{timestamp}{fileExtension}";
+
+                    var directoryPath = _configuration.GetValue<string>("ImagePath:DirectoryPath");
+
+                    var path = Path.Combine(directoryPath, fileName);
+
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        updatedRoom.Image.CopyTo(stream);
+                    }
+
+                    room.Image = path;
+                }
+
+                await _roomService.UpdateRoomAsync(room);
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ HotelReservation.Api/Validation/RoomValidation.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using HotelReservation.Api.Models;
+using HotelReservation.Domain.ServiceInterfaces;
+
+namespace HotelReservation.Api.Validation
+{
+    public class RoomValidation : AbstractValidator<RoomDTO>
+    {
+        public RoomValidation(IHotelService hotelService)
+        {
+            RuleFor(x => x.RoomNumber)
+                .NotNull()
+                .NotEmpty().WithMessage("RoomNumber should not be empty")
+                .MaximumLength(10).WithMessage("RoomNumber maximum length 10 characters");
+
+            RuleFor(x => x.Type)
+                .IsInEnum().WithMessage("Invalid Room Type");
+
+            RuleFor(x => x.AdultCapacity)
+                .NotNull()
+                .NotEmpty().WithMessage("AdultCapacity should not be empty")
+                .GreaterThanOrEqualTo(0).WithMessage("AdultCapacity must be greater than 0");
+
+            RuleFor(x => x.ChildrenCapacity)
+                .NotNull()
+                .NotEmpty().WithMessage("ChildrenCapacity should not be empty")
+                .GreaterThanOrEqualTo(0).WithMessage("ChildrenCapacity must be greater than 0");
+
+            RuleFor(x => x.HotelId)
+                .NotEmpty().WithMessage("HotelId is required")
+                .GreaterThan(0).WithMessage("HotelId should be greater than 0")
+                .MustAsync(async (hotelId, cancellation) => await hotelService.HotelExists(hotelId))
+                .WithMessage("Hotel does not exist");
+        }
+    }
+}
